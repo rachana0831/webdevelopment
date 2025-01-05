4939ea5fbb3205e0e5e26f04200638c8273bef4b
rachana0831 <141640473+rachana0831@users.noreply.github.com>
2025-01-05 15:53:57 +0530

    Add files via upload

---
+++ level 1 task 3/script.js
@@ -0,0 +1,24 @@
+
+
+let display = document.getElementById("result");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteChar() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
